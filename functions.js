4378f6b48a55033dad966fe6f986b5b2024abe2e
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-04-25 15:56:36 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,96 @@
+/* função simples
+function imprimirNoConsole(){
+
+    console.log("OIIII");
+}
+
+imprimirNoConsole();
+
+// quando cria uma função e há algo dentro, quando chama a função, ele vai executar o que estiver dentro da função.
+
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+/* função por parametro
+function imprimirUmNumero(num){
+
+    console.log("Numero: " + num);
+}
+
+imprimirUmNumero(24);
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+/*const numeroAleatorio = function(){
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
+numeroAleatorio();
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+
+/*function multiplicarNumero(x,y,z){
+    return x * y * z;
+}
+
+console.log(multiplicarNumero(2,3,6));
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+/*function podeDirigir(idade,cnh){
+
+    if (idade>=18 && cnh == true){
+
+        console.log("Pode dirigir")
+    }else{
+        console.log("Não tem permissão para dirigir");
+    }
+}
+
+console.log(podeDirigir(19,true));
+console.log(podeDirigir(17,false));
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+/*Escopo de uma função (global,local), só faz isso com let
+
+let y = 10;
+
+function imprimir(){
+    let y = 150;
+    console.log(y)
+}
+
+imprimir();
+console.log(y);
+
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+/*let consoleTeste = () =>{
+    console.log("OIIIIII");
+}
+
+consoleTeste();
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
+
+/*let soma = (a,b) =>{
+    return a+b;
+
+}
+
+console.log(soma(1,2))
+*/
+
+/*------------------------------------------------------------------------------------------------------------------- */
