1d6dacba67fec84f9d7abfab913cab73e0777800
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-04-25 16:38:46 -0300

    continuar

---
+++ exercios2Funcao.js
@@ -0,0 +1,64 @@
+/* Exercicio 1
+
+function imprimir(){
+
+    console.log("Hello World");
+}
+
+
+imprimir();
+*/
+
+/*------------------------------------------------------------------ */
+
+/* Exercicio 2
+
+function imprimir(idade){
+
+    console.log(`Você tem ${idade} anos`);
+}
+
+imprimir(25);
+
+*/
+
+/*------------------------------------------------------------------ */
+
+/* Exercicio 3
+
+function imprimirSoma(a,b){
+
+    return a+b;
+}
+
+console.log(imprimirSoma(2,5));
+
+*/
+
+/*------------------------------------------------------------------ */
+
+//Exercicio 4
+
+//function numeroAleatorio()   (fazer depois)
+
+/*------------------------------------------------------------------ */
+
+/* Exercicio 5
+
+function imprimirIdade(idade){
+
+    if (idade >=18){
+
+        console.log("Você pode entrar na autoescola");
+    }else{
+
+        console.log("Você não pode entrar na autoescola");
+    }
+}
+
+imprimirIdade(18);
+imprimirIdade(15);
+
+*/
+
+/*------------------------------------------------------------------ */
